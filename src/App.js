d6ccafb93b49bb6b78aaf299e34f8103da89e6ea
GuruNathan11 <121783817+GuruNathan11@users.noreply.github.com>
2023-06-08 11:24:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,94 +1,3 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './App.css';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// function JobApplicationForm() {
-//   const [userName, setUserName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [mobile, setMobile] = useState('');
-//   const [experience, setExperience] = useState('');
-//   const [ctc, setCtc] = useState('');
-//   const [file, setFile] = useState(null);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-//       const formData = new FormData();
-//       formData.append('userName', userName);
-//       formData.append('email', email);
-//       formData.append('mobile', mobile);
-//       formData.append('experience', experience);
-//       formData.append('ctc', ctc);
-//       formData.append('file', file);
-//       await axios.post('https://ens-job-apply.onrender.com/api/upload', formData);
-//       toast.success('Successfully applied for the job!', {
-//         position: toast.POSITION.TOP_CENTER,
-//         autoClose: 3000, // 3 seconds
-//       });
-//       // Reset form fields
-//       setUserName('');
-//       setEmail('');
-//       setMobile('');
-//       setExperience('');
-//       setCtc('');
-//       setFile(null);
-//     } catch (error) {
-//       toast.error('Failed to apply for the job!', {
-//         position: toast.POSITION.TOP_CENTER,
-//         autoClose: 3000, // 3 seconds
-//       });
-//     }
-//   };
-
-//   return (
-//     <div className='form-container'>
-//       <h1 className='form-heading'>Elon Native System</h1>
-//       <h2 className='form-subheading'>Job Application</h2>
-//       <div className='form-box'>
-//         <form onSubmit={handleSubmit} className='job-form'>
-//           <div className='form-group'>
-//             <label className='form-label'>User Name:</label>
-//             <input className='form-input' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='Enter your Full Name' />
-//           </div>
-//           <div className='form-group'>
-//             <label className='form-label'>Email:</label>
-//             <input className='form-input' type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Active Email Address' />
-//           </div>
-//           <div className='form-group'>
-//             <label className='form-label'>Mobile:</label>
-//             <input className='form-input' type='tel' value={mobile} onChange={(e) => setMobile(e.target.value)} placeholder='Active Mobile Number' />
-//           </div>
-//           <div className='form-group'>
-//             <label className='form-label'>Experience:</label>
-//             <select className='form-input' value={experience} onChange={(e) => setExperience(e.target.value)}>
-//               <option value=''>Select Experience</option>
-//               <option value='1 Year'>1 Year</option>
-//               <option value='2 Years'>2 Years</option>
-//               <option value='3 Years'>3 Years</option>
-//               {/* Add more options for different experience levels */}
-//             </select>
-//           </div>
-//           <div className='form-group'>
-//             <label className='form-label'>CTC:</label>
-//             <input className='form-input' type='text' value={ctc} onChange={(e) => setCtc(e.target.value)} placeholder='Expected CTC in Lahks' />
-//           </div>
-//           <div className='form-group'>
-//             <label className='form-label'>Resume:</label>
-//             <input className='form-input' type='file' onChange={(e) => setFile(e.target.files[0])} />
-//           </div>
-//           <button type='submit' className='form-submit'>Submit</button>
-//         </form>
-//       </div>
-//       <ToastContainer />
-//     </div>
-//   );
-// }
-
-// export default JobApplicationForm;
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
@@ -99,10 +8,12 @@ function JobApplicationForm() {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [mobile, setMobile] = useState('');
-  // const [role, setRole] = useState('');
   const [experience, setExperience] = useState('');
   const [ctc, setCtc] = useState('');
   const [file, setFile] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -110,40 +21,47 @@ function JobApplicationForm() {
     if (!userName || !email || !mobile || !experience || !ctc || !file) {
       toast.error('Please fill in all the fields!', {
         position: toast.POSITION.TOP_CENTER,
-        autoClose: 3000, // 3 seconds
+        autoClose: 3000,
       });
       return;
     }
+
+    setIsSubmitting(true);
 
     try {
       const formData = new FormData();
       formData.append('userName', userName);
       formData.append('email', email);
       formData.append('mobile', mobile);
-      // formData.append('role', role);
       formData.append('experience', experience);
       formData.append('ctc', ctc);
       formData.append('file', file);
+
       await axios.post('https://ens-job-apply.onrender.com/api/upload', formData);
+
       toast.success('Successfully applied for the job!', {
         position: toast.POSITION.TOP_CENTER,
-        autoClose: 3000, // 3 seconds
+        autoClose: 3000,
       });
-      // Reset form fields
+
       setUserName('');
       setEmail('');
       setMobile('');
-      // setRole('');
       setExperience('');
       setCtc('');
       setFile('');
+
+      document.getElementById('file-input').value = '';
     } catch (error) {
       toast.error('Failed to apply for the job!', {
         position: toast.POSITION.TOP_CENTER,
-        autoClose: 3000, // 3 seconds
+        autoClose: 3000,
       });
+    } finally {
+      setIsSubmitting(false);
     }
   };
+
 
   return (
     <div className='form-container'>
@@ -196,9 +114,11 @@ function JobApplicationForm() {
           </div>
           <div className='form-group'>
             <label className='form-label'>Resume:</label>
-            <input className='form-input' type='file' onChange={(e) => setFile(e.target.files[0])} />
+            <input id = 'file-input' className='form-input' type='file' onChange={(e) => setFile(e.target.files[0])} />
           </div>
-          <button type='submit' className='form-submit'>Submit</button>
+          <button type='submit' className='form-submit' disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+            </button>
         </form>
       </div>
       <ToastContainer />
